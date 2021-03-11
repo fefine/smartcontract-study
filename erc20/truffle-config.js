c76da6c1193eae00161943a7cc6d19a787168be1
fefine <feng_sh@outlook.com>
2021-03-11 09:35:21 +0800

    update ft

--- erc20/truffle-config.js
+++ erc20/truffle-config.js
@@ -16,7 +16,28 @@ module.exports = {
       network_id: 4,
       gas: 3000000,
       gasPrice: 10000000000
-    }
+    },
+    goerli: {
+      provider: () => new HDWalletProvider("moral wall festival system beyond tool abstract neglect slam account negative clock", "https://goerli.infura.io/v3/762b6ebf3ca247fbaf63b7bf33571a5c"),
+      network_id: 5,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    mumbai: {
+      provider: () => new HDWalletProvider("moral wall festival system beyond tool abstract neglect slam account negative clock", "https://rpc-mumbai.matic.today"),
+      network_id: 80001,
+      gas: 3000000,
+      gasPrice: 10000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    matic: {
+      provider: () => new HDWalletProvider("moral wall festival system beyond tool abstract neglect slam account negative clock", "https://rpc-mainnet.matic.network"),
+      network_id: 137,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
   },
   compilers: {
     solc: {
